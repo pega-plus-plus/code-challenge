49300d7a31c1522dcaf19170504315f24f6064b8
Chris West <west@USF-Gold-Wifi-226-62-81.laptops.usf.edu>
2019-02-25 14:31:54 -0500

    Updating Dao and Imp.

--- West_Code/FlashCard/src/main/java/com/flashcard/dao/ViewFlashCardDao.java
+++ West_Code/FlashCard/src/main/java/com/flashcard/dao/ViewFlashCardDao.java
@@ -1,7 +1,11 @@
 package com.flashcard.dao;
 
+import java.util.List;
+
+import com.flashcard.bean.FlashCard;
+
 public interface ViewFlashCardDao {
 
-	public abstract void viewFlashCards();
-	public abstract void viewCategory();
+	public abstract List<FlashCard> viewFlashCards();
+	public abstract List<FlashCard> viewCategory(String category);
 }
--- West_Code/FlashCard/src/main/java/com/flashcard/daoimp/ViewFlashCardDaoImp.java
+++ West_Code/FlashCard/src/main/java/com/flashcard/daoimp/ViewFlashCardDaoImp.java
@@ -1,20 +1,60 @@
 package com.flashcard.daoimp;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.flashcard.bean.FlashCard;
 import com.flashcard.dao.ViewFlashCardDao;
 import com.flashcard.utilities.ConnFactory;
 
 public class ViewFlashCardDaoImp implements ViewFlashCardDao {
-	public static ConnFactory con = ConnFactory.getInstance();
+	public static ConnFactory conF = ConnFactory.getInstance();
+
 	@Override
-	public void viewFlashCards() {
-		// TODO Auto-generated method stub
-		
+	public List<FlashCard> viewFlashCards() {
+		List<FlashCard> flashCards = new ArrayList<FlashCard>();
+		try (Connection conn = conF.getConnection()) {
+			String sql = "SELECT * FROM FlashCard";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			FlashCard flash = null;
+			while (rs.next()) {
+				flash = new FlashCard(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));
+				flashCards.add(flash);
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		return flashCards;
+
 	}
 
 	@Override
-	public void viewCategory() {
-		// TODO Auto-generated method stub
+	public List<FlashCard> viewCategory(String category) {
+		List<FlashCard> flashCards = new ArrayList<FlashCard>();
+		try (Connection conn = conF.getConnection()){
+			String sql = "SELECT * FROM FlashCard WHERE category2 = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			FlashCard flash = null;
+			while (rs.next()) {
+				flash = new FlashCard(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));
+				flashCards.add(flash);
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
 		
+		
+		return flashCards;
+		
+
 	}
 
 }
